217839c168a8e49e7c02e3132f9d57d577fb1411
Salman MP <mpsalman2011@gmail.com>
2025-03-11 01:11:11 +0530

    vite config and env updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
+// import { defineConfig } from 'vite';
+// import preact from '@preact/preset-vite';
+
+// const config = defineConfig({
+//   plugins: [preact()],
+//   base: './',
+// });
+
+// export default config;
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 const config = defineConfig({
   plugins: [preact()],
   base: './',
+  server: {
+    port: 11347, // Set your custom port here
+    allowedHosts: ['.digibayt.com'], // Add your allowed host here
+  },
 });
 
 export default config;
